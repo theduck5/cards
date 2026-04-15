2ca6bc4e10ab4b183b5e4da526b2996a7808fb58
theduck5 <foxyfamily111@gmail.com>
2026-04-15 14:07:25 -0700

    3
    DONT RESS SPACE

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager gm;
-    public List<Card> deck = new List<Card>();
-    public List<Card> player_deck = new List<Card>();
-    public List<Card> ai_deck = new List<Card>();
-    public List<Card> player_hand = new List<Card>();
-    public List<Card> ai_hand = new List<Card>();
-    public List<Card> discard_pile = new List<Card>();
+    public List<Card_data> deck = new List<Card_data>();
+    public List<Card_data> player_deck = new List<Card_data>();
+    public List<Card_data> ai_deck = new List<Card_data>();
+    public List<Card_data> player_hand = new List<Card_data>();
+    public List<Card_data> ai_hand = new List<Card_data>();
+    public List<Card_data> discard_pile = new List<Card_data>();
+    [SerializeField] Card blank;
+    [SerializeField] Canvas canvas;
 
     private void Awake()
     {
@@ -38,12 +41,33 @@ public class GameManager : MonoBehaviour
 
     void Deal()
     {
+        if (player_deck.Count > 0)
+        {
+            int NewCard = (int) RNG(0,player_deck.Count-1);
+            player_hand.Add(player_deck[NewCard]);
 
+            Card AddCard = Instantiate(blank,new(0,0,0),Quaternion.identity,canvas.transform);
+            AddCard.data = player_deck[NewCard];
+            AddCard.name = AddCard.data.card_name;
+            player_deck.Remove(player_deck [NewCard]);
+        }
+       else
+        {
+            Shuffle();
+        }
     }
 
     void Shuffle()
     {
+        print("EVERYDAY IM SHUFFLING");
 
+        for (int i = discard_pile.Count - 1; i > 0; i--)
+        {
+            int NewCard = (int)RNG(0,discard_pile.Count - 1);
+            deck.Add(discard_pile[NewCard]);
+            discard_pile.Remove(discard_pile[NewCard]);
+        }
+        Deal();
     }
 
     void AI_Turn()
@@ -51,7 +75,18 @@ public class GameManager : MonoBehaviour
 
     }
 
+    public float RNG(float min,float max)
+    {
+        return UnityEngine.Random.Range(min,max);
+    }
 
+    public void spacebar(InputAction.CallbackContext Context)
+    {
+        if (Context.performed)
+        {
+            Deal();
+        }
+    }
 
     
 }
